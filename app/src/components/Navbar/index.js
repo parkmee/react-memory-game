9a5e84c680f54acd641d8f222f53504d3834baf3
MMPark <parkmee@gmail.com>
2019-01-10 00:50:09 -0500

    initial commit; navbar

---
+++ app/src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () =>
+  <div>
+    <div className="navbar bg-dark row sticky-top shadow">
+      <div className="nav-section col-4 text-center">
+        Clicky Game
+      </div>
+      <div className="nav-section col-4 text-center">
+        Click to Start!
+      </div>
+      <div className="nav-section col-4 text-center">
+        Score: 0 | Top Score: 0
+      </div>
+    </div>
+  </div>
+
+export default Navbar;
