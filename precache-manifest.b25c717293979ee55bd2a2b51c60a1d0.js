13c4f67936194f86630a2f54ddc63920ef74f71a
MMPark <parkmee@gmail.com>
2019-01-10 23:55:23 -0500

    Updates

---
+++ precache-manifest.b25c717293979ee55bd2a2b51c60a1d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "0fa0b24e08e91b11395c",
+    "url": "/react-memory-game/static/js/main.0fa0b24e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-memory-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0fa0b24e08e91b11395c",
+    "url": "/react-memory-game/static/css/main.35f50ae2.chunk.css"
+  },
+  {
+    "revision": "701c6862007c793febcae27c5d25b706",
+    "url": "/react-memory-game/index.html"
+  }
+];
